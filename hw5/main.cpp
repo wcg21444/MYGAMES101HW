4cd35d1e8089eb68b79836e96c8154366a9060f0
wcg21444 <1789810694@qq.com>
2025-06-28 11:00:17 +0800

    hw5 implemented multi thread rendering

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -12,17 +12,23 @@ int main()
     Scene scene(1280, 960);
 
     auto sph1 = std::make_unique<Sphere>(Vector3f(-1, 0, -12), 2);
-    sph1->materialType = DIFFUSE_AND_GLOSSY;
+    sph1->materialType = REFLECTION_AND_REFRACTION;
     sph1->diffuseColor = Vector3f(0.6, 0.7, 0.8);
 
     auto sph2 = std::make_unique<Sphere>(Vector3f(0.5, -0.5, -8), 1.5);
     sph2->ior = 1.5;
     sph2->materialType = REFLECTION_AND_REFRACTION;
 
+    auto sph3 = std::make_unique<Sphere>(Vector3f(2, 0, -8), 1.5);
+    sph3->ior = 1.6;
+    sph3->diffuseColor = Vector3f(0.2, 0.7, 0.8);
+    sph3->materialType = REFLECTION_AND_REFRACTION;
+
     scene.Add(std::move(sph1));
     scene.Add(std::move(sph2));
+    scene.Add(std::move(sph3));
 
-    Vector3f verts[4] = {{-5,-3,-6}, {5,-3,-6}, {5,-3,-16}, {-5,-3,-16}};
+    Vector3f verts[4] = {{-5, -3, -6}, {5, -3, -6}, {5, -3, -16}, {-5, -3, -16}};
     uint32_t vertIndex[6] = {0, 1, 3, 1, 2, 3};
     Vector2f st[4] = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
     auto mesh = std::make_unique<MeshTriangle>(verts, vertIndex, 2, st);
@@ -30,7 +36,7 @@ int main()
 
     scene.Add(std::move(mesh));
     scene.Add(std::make_unique<Light>(Vector3f(-20, 70, 20), 0.5));
-    scene.Add(std::make_unique<Light>(Vector3f(30, 50, -12), 0.5));    
+    scene.Add(std::make_unique<Light>(Vector3f(30, 50, -12), 0.5));
 
     Renderer r;
     r.Render(scene);
